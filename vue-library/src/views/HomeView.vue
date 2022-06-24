cf1577bdb42bf672690ad192ce9b7eefb10061cf
brenonpaul <bp.brenon@gmail.com>
2022-06-24 17:18:07 -0300

    project start, vue-library directory creation

---
+++ vue-library/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Bookstore</h1>
+    <h2>Build a landpage</h2>
+  </main>
+</template>
