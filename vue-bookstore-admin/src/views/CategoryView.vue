2284a4b9a47f27a703fce796a46d235098de2965
brenonpaul <bp.brenon@gmail.com>
2022-06-27 23:36:21 -0300

    initial category layout

--- vue-bookstore-admin/src/views/CategoryView.vue
+++ vue-bookstore-admin/src/views/CategoryView.vue
@@ -1,15 +1,92 @@
 <template>
-  <div class="category">
-    <h1>This is an category page</h1>
-  </div>
+  <main>
+    <form>
+      <input type="text">
+      <button>Adicionar</button>
+    </form>
+    <div class="div-table">
+      <table>
+        <thead>
+          <th class="border-rad-l">ID</th>
+          <th class="border-rad-r">Descrição</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>teste</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>teste 2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </main>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .category {
-    min-height: 100vh;
+
+  main {
     display: flex;
-    align-items: center;
+    flex-flow: column;
   }
-}
+
+  form {
+    display: flex;
+    justify-content: center;
+    margin-top: 2%;
+    height: 10%;
+  }
+
+  form > input {
+    width: 30%;
+    height: 60%;
+    border: 0;
+    border-radius: 50px 0 0 50px;
+  }
+
+  form > button {
+    width: 6%;
+    height: 60%;
+    border: 0;
+    border-radius: 0 50px 50px 0;
+    background: rgb(2,0,36);
+    color: whitesmoke;
+  }
+  form > button:hover {
+    border-radius: 0 50px 50px 0;
+    background: rgba(16,100,117,1);
+    color: whitesmoke;
+  }
+
+  .div-table {
+    display: flex;
+    justify-content: center;
+  }
+
+  .div-table > table {
+    width: 40%;
+    text-align: center;
+    border-spacing: 0;
+  }
+
+  .div-table > table > thead {
+    background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(18,18,48,1) 35%, rgba(16,100,117,1) 100%);
+    color: whitesmoke;
+  }
+
+  .border-rad-l {
+    border-radius: 10px 0 0 0 ;
+  }
+
+  .border-rad-r {
+    border-radius: 0 10px 0 0 ;
+  }
+
+  tbody {
+    background: rgb(192, 244, 255);
+    color: rgb(24, 22, 22);
+  }
 </style>
