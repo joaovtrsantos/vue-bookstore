eacb3f4ccd97c604ece2f533342da8e4709d3073
João Vitor dos Santos <jsantos@isolutions.com.br>
2022-07-07 15:46:37 -0300

    Atualização home

--- vue-bookstore-client/src/views/HomeView.vue
+++ vue-bookstore-client/src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Booksto</h1>
   </main>
 </template>
+<style>
+
+  h1 {
+      font-weight: 500;
+      font-size: 2.6rem;
+      top: -10px;
+      margin-top: 3%;
+      text-align: center;
+    }
+</style>
